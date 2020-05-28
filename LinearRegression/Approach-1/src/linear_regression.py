ac8d19fab8925e3be78000b7f00cee64d272f552
davy-datascience <davy.datascience@gmail.com>
2020-05-28 19:48:42 +0200

    complete linear regression approach 1

--- LinearRegression/Approach-1/src/linear_regression.py
+++ LinearRegression/Approach-1/src/linear_regression.py
@@ -1,69 +1,57 @@
 import pandas as pd
 from my_functions import drawAll, transformLine, predict
 from sympy.geometry import Point, Line
-import time
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+import progressbar
 
-learning_rate = 0.005
+# Set the learning rate and the number of iterations
+learning_rate = 0.01
 nb_epochs = 1000
 
 # Read the data
-dataset = pd.read_csv("Salary_Data.csv")
+dataset = pd.read_csv("https://raw.githubusercontent.com/davy-datascience/portfolio/master/LinearRegression/Approach-1/dataset/Salary_Data.csv")
 
-# arrondi à décimale
-dataset = dataset.round(0)
-
-#Suppression des lignes sans valeurs 
-dataset.dropna(axis=0, subset=['Salary'], inplace=True)
-
-#Add indexes (no indexes in that dataset)
-indexes_dataset = [i for i in range(len(dataset))]
-dataset = dataset.assign(index=pd.Series(indexes_dataset).values)
-dataset = dataset[['index', 'YearsExperience', 'Salary']]
-dataset.set_index('index')
-
-#Séparation variable prédictive - variable dépendante
+# Separation independent variable X - dependent variable y
 X = dataset.YearsExperience
 y = dataset.Salary
 
+# Separate the dataset into a training set and a test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Cherche 1ère droite équation 
+# Looking for 1st line equation
+# The line must have the same scope than the scatter plots from the dataset
+# I decided to build the line choosing the point that has the max x-value and the point that has the min x-value
 
-# Cherche point qui a le x au max
+# Find the point with the maximum value of x in the dataset
 idx_max = X_train.idxmax()
 x_max = Point(X_train.loc[idx_max], y_train.loc[idx_max])
 
-# Cherche point qui a le x au min
+# Find the point with the minimum value of x in the dataset
 idx_min = X_train.idxmin()
 x_min = Point(X_train.loc[idx_min], y_train.loc[idx_min])
 
-# vecteur droite pour équation 
-#x_max - x_min
+# Build the line with the 2 points 
 line = Line(x_min, x_max)
 drawAll(X_train, y_train, line)
 
-start_time = time.time()
-
-for i in range(nb_epochs):
+# Iterate choosing a random point and moving the line with the function transformLine
+for i in progressbar.progressbar(range(nb_epochs)):
     sample = dataset.sample()
     point = Point(sample.YearsExperience, sample.Salary)
     line = transformLine(point, line, X_train.median(), learning_rate)
-    #drawAll(X_train, y_train, line)
+    #drawAll(X_train, y_train, line)    # Uncomment this line to see the line at each iteration
 
 drawAll(X_train, y_train, line)
 
+# Predict the test set with my model and see
 y_pred = predict(X_test, line)
-print("MAE my model: {}".format(mean_absolute_error(y_pred, y_test)))
+print("MAE (Mean Absolute Error) is used to evaluate the model accuracy")
+print("MAE for my model: {}".format(mean_absolute_error(y_pred, y_test)))
 
-
+# Predict the test set with the sklearn algorithm
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train.to_frame(), y_train)
-
-# Faire de nouvelles prédictions
 y_pred2 = regressor.predict(X_test.to_frame())
-print("MAE Linear Regression: {}".format(mean_absolute_error(y_pred2, y_test)))
-
-print("--- TOTAL --- %s seconds ---" % (time.time() - start_time))
+print("MAE for the algorithm of the sklearn module: {}".format(mean_absolute_error(y_pred2, y_test)))
--- LinearRegression/Approach-1/src/my_functions.py
+++ LinearRegression/Approach-1/src/my_functions.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from sympy.geometry import Point, Line
 
 def drawAll(X, Y, line):
+    """ plot the points from the dataset and draw the actual Line """
     coefs = line.coefficients
     x = np.linspace(X.min(),X.max())
     y = (-coefs[0] * x - coefs[2]) / coefs[1]
@@ -11,13 +12,14 @@ def drawAll(X, Y, line):
     plt.show()
 
 def transformLine(point, line, x_median, learning_rate): 
-    #On prend la médiane des valeurs de x pour de meilleurs résultats pour les calculs des distances horizontales
+    """ According to the random point, update the Line """
+    # We take the median of the x values for better results for the calculations of the horizontal distances
     
-    #Création de la ligne verticale qui passe par le nouveau point
+    # Creation of the vertical line passing through the new point
     ymin = line.points[0] if line.direction.y > 0 else line.points[1]
     ymax = line.points[1] if line.direction.y > 0 else line.points[0]
     vertical_line = Line(Point(point.x,ymin.y), Point(point.x,ymax.y))
-    #Cherche l'intersection avec notre droite (pour calculer la distance verticale)
+    # Find the intersection with our line (to calculate the vertical distance)
     I = line.intersection(vertical_line)
     vertical_distance = point.y - I[0].y
     horizontal_distance = point.x - x_median  
@@ -28,19 +30,24 @@ def transformLine(point, line, x_median, learning_rate):
     b = coefs[1]
     c = coefs[2]
     
-    #Calcul des points qui constituent la nouvelle ligne
-    # y = - (a/b)*x + learning_rate * vertical_distance * horizontal_distance - (c/b) + learning_rate * vertical_distance
+    # Calculation of the points which constitute the new line
+    # Reminder: we add (learning_rate * vertical_distance * horizontal_distance) to the slope and we add (learning_rate * vertical_distance) to y-intercept
+    # The equation now looks like : 
+    # y = - (a/b)*x + (learning_rate * vertical_distance * horizontal_distance) * x - (c/b) + learning_rate * vertical_distance
+
+    # We keep the same scope of the line so the min value of x and the max value of x don't change
     
     x_min = line.points[0].x
-    y_min = - (a/b)*x_min + (learning_rate * vertical_distance * horizontal_distance * x_min) /b - (c/b) + learning_rate * vertical_distance
+    y_min = - (a/b)*x_min + (learning_rate * vertical_distance * horizontal_distance * x_min) - (c/b) + learning_rate * vertical_distance
     
     x_max = line.points[1].x
-    y_max = - (a/b)*x_max + (learning_rate * vertical_distance * horizontal_distance * x_max) /b - (c/b) + learning_rate * vertical_distance
+    y_max = - (a/b)*x_max + (learning_rate * vertical_distance * horizontal_distance * x_max) - (c/b) + learning_rate * vertical_distance
     
     newLine = Line(Point(x_min, y_min), Point(x_max, y_max))
     return newLine
     
 def predict(X, line):
+    """ I use my model (the equation of the line) to predict new values """
     prediction = []
     coefs = line.coefficients
     a = coefs[0]
