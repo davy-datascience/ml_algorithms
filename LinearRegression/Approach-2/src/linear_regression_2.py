a62c1bff9b986d71137eb0dbecb15fca767fdc53
davy-datascience <davy.datascience@gmail.com>
2020-06-08 18:20:18 +0200

    linear regression approach 2 refactor

--- LinearRegression/Approach-2/src/linear_regression_2.py
+++ LinearRegression/Approach-2/src/linear_regression_2.py
@@ -3,9 +3,11 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from my_functions import normalize, feature_engineering, predict
+import progressbar
 
+# Set the learning rate and the number of iterations
 learning_rate = 0.1
-epochs = 5
+epochs = 50
 
 # Read the data
 dataset = pd.read_csv("https://raw.githubusercontent.com/davy-datascience/portfolio/master/LinearRegression/Approach-2/dataset/house_pricing.csv")
@@ -13,6 +15,8 @@ dataset = pd.read_csv("https://raw.githubusercontent.com/davy-datascience/portfo
 # Separate the dataset into a training set and a test set
 train, test = train_test_split(dataset, test_size = 0.2)
 
+# Separation independent variable X - dependent variable y for the train set & the test set
+# Do specific feature engineering & normalize X
 X_train = feature_engineering(train)
 X_train = normalize(X_train)
 y_train = train["SalePrice"]
@@ -21,35 +25,29 @@ X_test = feature_engineering(test)
 X_test = normalize(X_test)
 y_test = test["SalePrice"]
 
+# Arbitrary begin with all tetas = 1
 tetas = np.ones(X_train.columns.size + 1)
+
+for i in progressbar.progressbar(range(epochs)):
+    # Create a variable new_tetas so we can update simulatneously variable tetas
+    new_tetas = []
     
-def regressionFunction(tetas, X):
-    X = pd.Series([1]).append(X)
-    return tetas.dot(X)
+    # Iterate over all tetas to calcuate their new values that will be updated simulteously
+    for j in range(len(tetas)):
+        sum_cost = []
+        # Iterate over all training data
+        for i in range(len(X_train.index)):
+            # Calculate the predicted value of the data row with our regressionFunction
+            h = predict(X_train.iloc[i], tetas)
+            y = y_train.iloc[i]
+            x = 1
+            if j != 0:
+                x = X_train.iloc[i, j - 1]
+            sum_cost.append((h - y) * x)
+        new_tetas.append(tetas[j] - learning_rate * (1/len(X_train.index)) * sum(sum_cost))
 
-def gradientDescent(X, epochs, learning_rate):
-    tetas = np.ones(X.columns.size + 1)
-    
-    for epoch in range(epochs):
-        new_tetas = []
-        
-        for j in range(len(tetas)):
-            sum_cost = []
-            for i in range(len(X.index)):
-                h = regressionFunction(tetas, X_train.iloc[i])
-                y = y_train.iloc[i]
-                x = 1
-                if j != 0:
-                    x = X.iloc[i, j - 1]
-                sum_cost.append((h - y) * x)
-            new_tetas.append(tetas[j] - learning_rate * (1/len(X.index)) * sum(sum_cost))
-
-        tetas = np.array(new_tetas)
-        print(tetas)
-        
-    return tetas
-
-tetas = gradientDescent(X_train, epochs, learning_rate)
+    # Simultaneous update of thetas
+    tetas = np.array(new_tetas)
 
 # Predict the test set with my model and see
 y_pred = predict(X_test, tetas)
--- LinearRegression/Approach-2/src/my_functions.py
+++ LinearRegression/Approach-2/src/my_functions.py
@@ -1,15 +1,19 @@
 import pandas as pd
+from pandas.core.frame import DataFrame
 
 def normalize(X):
+    ''' Apply mean normalization, so each feature is at the same scale '''
     X_normalized = pd.DataFrame()
+    avg_X = X.mean()
+    range_X = X.max() - X.min()
+    
     for i in range(len(X.columns)):
-        avg_x = X.mean()[i] #  on peut le sortir du for et faire un array avc ttes les moyennes
-        range_x = X.max()[i] - X.min()[i]
-        X_normalized[i] = (X.iloc[:, i] - avg_x) / range_x
+        X_normalized[i] = (X.iloc[:, i] - avg_X[i]) / (range_X[i] if range_X[i] != 0 else 1)
     X_normalized.columns = X.columns
     return X_normalized
 
 def feature_engineering(X):
+    ''' Select interesting features and create new calculated features '''
     features = ['LotArea', '1stFlrSF', '2ndFlrSF', 'BedroomAbvGr', 'KitchenAbvGr', 'FullBath', 'HalfBath', 'BsmtFullBath', 'BsmtHalfBath', 'PoolArea']
     
     X = X[features].copy()
@@ -21,8 +25,22 @@ def feature_engineering(X):
     return X
 
 def predict(X, tetas):
-    y_pred = X.iloc[:, 0] * tetas[1] + tetas[0]
-    for i in range(2, len(tetas)):
-        y_pred += X.iloc[:, i-1] * tetas[i] 
+    ''' Predict with the model (tetas)
+        X input can be either a Series for a single example to predict or a DataFrame
+    '''
+    X = X.copy() # So we do not alter the input dataframe X
+    
+    # Check if the input is either DataFrame or a Series
+    if isinstance(X, DataFrame):
+        X.insert(0, "X0", 1)
+        y_pred = pd.Series()
         
-    return(y_pred)
+        for i in range(len(X.index)):
+            y_pred = y_pred.append(pd.Series(tetas.dot(X.iloc[i])))
+        
+        return y_pred
+    
+    else:
+        X = pd.Series([1]).append(X)
+        return tetas.dot(X)
+        
